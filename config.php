0dd9a8bc664585ac90be4fbd4eeb31dd165b8e72
Gabe Hollombe <gabe@avantbard.com>
2020-07-14 11:26:11 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+  $host       = $_SERVER['RDS_HOSTNAME'];
+  $username   = $_SERVER['RDS_USERNAME'];
+  $password   = $_SERVER['RDS_PASSWORD'];;
+  $dsn        = "mysql:host=$host;dbname=tasks";
+  $options    = array(
+                  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                );
